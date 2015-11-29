0529609370983ca9f21c3a17d7f4afd3eecfd818
John Kerry <jk185160@ncr.com>
2015-11-28 23:28:32 -0500

    upgrading the version to 0.5

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'john@kerryhouse.net'
 license          'All rights reserved'
 description      'Installs/Configures sftp_provider'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.5.0'
